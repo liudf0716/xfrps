9e5153cc6a6d06eacb39f4a7ce208633db6add09
Dengfeng Liu <liudf0716@qq.com>
2017-06-20 14:50:06 +0800

    Update consts.go

--- models/consts/consts.go
+++ models/consts/consts.go
@@ -23,8 +23,9 @@ var (
 	Offline string = "offline"
 
 	// proxy type
-	TcpProxy   string = "tcp"
-	UdpProxy   string = "udp"
-	HttpProxy  string = "http"
-	HttpsProxy string = "https"
+	TcpProxy   	string = "tcp"
+	UdpProxy   	string = "udp"
+	HttpProxy  	string = "http"
+	HttpsProxy 	string = "https"
+	FtpProxy	string = "ftp"
 )
