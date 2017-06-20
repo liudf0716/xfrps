e7d0c976184c54a866ab4529a0070be8ff4f06f2
Dengfeng Liu <liudf0716@qq.com>
2017-06-20 14:41:41 +0800

    Update server_common.go

--- models/config/server_common.go
+++ models/config/server_common.go
@@ -231,10 +231,10 @@ func LoadServerCommonConf(conf ini.File) (cfg *ServerCommonConf, err error) {
 	}
 
 	tmpStr, ok = conf.Get("common", "tcp_mux")
-	if ok && tmpStr == "false" {
-		cfg.TcpMux = false
-	} else {
+	if ok && tmpStr == "true" {
 		cfg.TcpMux = true
+	} else {
+		cfg.TcpMux = false
 	}
 
 	tmpStr, ok = conf.Get("common", "heartbeat_timeout")
