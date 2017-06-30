d16cb9777e1073fe87edf424fedad7718bd1f310
Dengfeng Liu <liudf0716@qq.com>
2017-06-30 18:25:00 +0800

    Update index.js

--- web/frps/src/router/index.js
+++ web/frps/src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Overview from '../components/Overview.vue'
 import ProxiesTcp from '../components/ProxiesTcp.vue'
 import ProxiesUdp from '../components/ProxiesUdp.vue'
+import ProxiesFtp from '../components/ProxiesFtp.vue'
 import ProxiesHttp from '../components/ProxiesHttp.vue'
 import ProxiesHttps from '../components/ProxiesHttps.vue'
 
@@ -21,6 +22,10 @@ export default new Router({
         path: '/proxies/udp',
         name: 'ProxiesUdp',
         component: ProxiesUdp
+    }, {
+        path: '/proxies/ftp',
+        name: 'ProxiesFtp',
+        component: ProxiesFtp
     }, {
         path: '/proxies/http',
         name: 'ProxiesHttp',
