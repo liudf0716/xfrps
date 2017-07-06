2db94de5f14a22fd7562cc130dcdee26f46f5b66
Dengfeng Liu <liudf0716@qq.com>
2017-07-06 13:54:25 +0800

    Update dashboard.go

--- server/dashboard.go
+++ server/dashboard.go
@@ -47,6 +47,7 @@ func RunDashboardServer(addr string, port int64) (err error) {
 	router.GET("/api/proxy/https", httprouterBasicAuth(apiProxyHttps))
 	router.GET("/api/proxy/traffic/:name", httprouterBasicAuth(apiProxyTraffic))
 	router.GET("/api/getfreeport/:proto", httprouterNoAuth(apiGetFreePort))
+	router.GET("/api/tcp/isportfree/:port", httprouterNoAuth(apiIsTcpPortFree))
 
 	// view
 	router.Handler("GET", "/favicon.ico", http.FileServer(assets.FileSystem))
