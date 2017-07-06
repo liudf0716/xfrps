a32efe561d15292a744402fda35024fdc2dc22e0
Dengfeng Liu <liudf0716@qq.com>
2017-07-06 11:22:44 +0800

    Update dashboard_api.go

--- server/dashboard_api.go
+++ server/dashboard_api.go
@@ -21,6 +21,7 @@ import (
 	"github.com/KunTengRom/xfrps/models/config"
 	"github.com/KunTengRom/xfrps/models/consts"
 	"github.com/KunTengRom/xfrps/utils/log"
+	"github.com/KunTengRom/xfrps/utils/util"
 	"github.com/KunTengRom/xfrps/utils/version"
 
 	"github.com/julienschmidt/httprouter"
