356eb1204b826203891dd2acebc159e55cb486dd
Dengfeng Liu <liudf0716@qq.com>
2017-08-22 17:47:59 +0800

    Update run_test.sh

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-./../bin/frps -c ./conf/auto_test_frps.ini &
+./../bin/xfrps -c ./conf/auto_test_frps.ini &
 sleep 1
-./../bin/frpc -c ./conf/auto_test_frpc.ini &
+./../bin/xfrpc -c ./conf/auto_test_frpc.ini &
 
 # wait until proxies are connected
 sleep 2
