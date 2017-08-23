69ee649e06bf2a52864df820d44791fcbbd04739
liudf0716 <liudengfeng@kunteng.org>
2017-08-23 13:54:17 +0800

    update dashboard to xfrps'info

--- web/frps/webpack.config.js
+++ web/frps/webpack.config.js
@@ -74,11 +74,11 @@ module.exports = (options = {}) => ({
         })
     ],
     devServer: {
-        host: '192.168.158.91',
+        host: '127.0.0.1',
         port: 8010,
         proxy: {
             '/api/': {
-                target: 'http://192.168.158.91:8080',
+                target: 'http://127.0.0.1:8080',
                 changeOrigin: true,
                 pathRewrite: {
                     '^/api': ''
